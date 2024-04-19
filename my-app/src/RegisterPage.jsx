939363b90617e246009e92307d2b7475708affd3
Prasanth <prasanthpv30@gmail.com>
2024-04-19 12:45:29 +0530

    input field updated

--- my-app/src/RegisterPage.jsx
+++ my-app/src/RegisterPage.jsx
@@ -98,7 +98,7 @@ function RegisterPage() {
                     <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
                     <div class="mt-2">
                       <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                        <input type="text" name="password" value={password} id="password" onChange={(e)=>setpassword(e.target.value)} autocomplete="password" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Enter passsword"/>
+                        <input type="password" name="password" value={password} id="password" onChange={(e)=>setpassword(e.target.value)} autocomplete="password" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Enter passsword"/>
                       </div>
                     </div>
                   </div>
